f0443662573d31635cd142ae6f19b238d3df005c
djramesh <deepbaishya4385@gmail.com>
2025-10-07 18:11:19 +0530

    update-latest

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import ScrollToTop from "./components/ScrollToTop";
 import About from "./components/About";
 import Gallery from "./components/Gallery";
 import Contact from "./components/Contact";
-import PartnerLogos from "./components/partnering-logo";
+// import PartnerLogos from "./components/partnering-logo";
 function App() {
   return (
     <div className="app-container">
